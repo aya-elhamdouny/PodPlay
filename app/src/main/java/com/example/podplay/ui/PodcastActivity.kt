83e9b0ed14548b1f4eae4b7d2a03af8fbc8e6856
aya-elhamdouny <52864131+aya-elhamdouny@users.noreply.github.com>
2021-10-26 16:43:12 +0200

    -search wigdet as actionView

--- app/src/main/java/com/example/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/example/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,14 @@
 package com.example.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.widget.SearchView
 import com.example.podplay.R
 import com.example.podplay.repository.ItunesRepository
 import com.example.podplay.service.ItunesService
@@ -18,13 +24,58 @@ class PodcastActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
 
+
+
+
+    }
+
+
+    private fun performSearch(term : String){
         val itunesService = ItunesService.instance
         val repository = ItunesRepository(itunesService)
 
         GlobalScope.launch{
-            val result  = repository.searchByTerm("Android developer")
+            val result  = repository.searchByTerm(term)
             Log.i(TAG , "RESULT = ${result.body()}")
         }
+    }
+
+
+    //2-check of intent action = search take string from intent and pass it to search action
+    private fun handleIntent(intent: Intent){
+        if(Intent.ACTION_SEARCH == intent.action){
+            val qurey = intent.getStringExtra(SearchManager.QUERY) ?:
+            return
+            performSearch(qurey)
+        }
+    }
+
+
+
+    //1-to be updated with search bar
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        if (intent != null) {
+            handleIntent(intent)
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search , menu)
+
+        //set el search as actionview
+        val searchmenuItem = menu?.findItem(R.id.search_item)
+        val searchview = searchmenuItem?.actionView as SearchView
+
+        //3ashan a3rf ageb el text aly fl search bar
+        val searchmanger = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        //assign el manger bl view
+        searchview.setSearchableInfo(searchmanger.getSearchableInfo(componentName))
+        return true
 
 
     }
