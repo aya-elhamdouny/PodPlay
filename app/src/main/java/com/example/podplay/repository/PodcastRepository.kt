e26fa4b6c23a58def75d6c2da26f813cbf190de6
aya-elhamdouny <52864131+aya-elhamdouny@users.noreply.github.com>
2021-10-29 22:34:32 +0200

    -add Rssfile using Okhhtp

--- app/src/main/java/com/example/podplay/repository/PodcastRepository.kt
+++ app/src/main/java/com/example/podplay/repository/PodcastRepository.kt
@@ -1,10 +1,51 @@
 package com.example.podplay.repository
 
+import com.example.podplay.model.Episode
 import com.example.podplay.model.Podcast
+import com.example.podplay.model.RssFeedResponse
+import com.example.podplay.service.RssFeedService
+import com.example.podplay.utils.DateUtils
 
-class PodcastRepository {
+class PodcastRepository(private var feedService: RssFeedService) {
+
+    private fun rssItemsToEpisodes(
+        episodeResponses: List<RssFeedResponse.EpisodeResponse>
+    ): List<Episode> {
+        return episodeResponses.map {
+            Episode(
+                it.guid ?: "",
+                it.title ?: "",
+                it.description ?: "",
+                it.url ?: "",
+                it.type ?: "",
+                DateUtils.xmlDateToDate(it.pubDate),
+                        it.duration ?: ""
+            )
+        }
+    }
+    private fun rssResponseToPodcast(
+        feedUrl: String, imageUrl: String, rssResponse:
+        RssFeedResponse
+    ): Podcast? {
+        val items = rssResponse.episodes ?: return null
+        val description = if (rssResponse.description == "")
+            rssResponse.summary else rssResponse.description
+        return Podcast(feedUrl, rssResponse.title, description,
+            imageUrl,
+            rssResponse.lastUpdated, episodes =
+            rssItemsToEpisodes(items))
+    }
 
 
-    fun getPodcast(feedUrl : String)  : Podcast? {
-        return Podcast(feedUrl, "no name", " no descreption", "no image") }
+
+
+
+    suspend fun getPodcast(feedUrl : String)  : Podcast? {
+        var podcast: Podcast? = null
+        val feedResponse = feedService.getFeed(feedUrl)
+        if (feedResponse != null) {
+            podcast = rssResponseToPodcast(feedUrl, "", feedResponse)
+        }
+        return podcast
+    }
 }
--- app/src/main/java/com/example/podplay/utils/DateUtils.kt
+++ app/src/main/java/com/example/podplay/utils/DateUtils.kt
@@ -20,4 +20,11 @@ object DateUtils {
                 Locale.getDefault())
         return outputFormat.format(date)
     }
+
+    fun xmlDateToDate(dateString: String?): Date {
+        val date = dateString ?: return Date()
+        val inFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z",
+            Locale.getDefault())
+        return inFormat.parse(date) ?: Date()
+    }
 }
